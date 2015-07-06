115521b7cf956380cbcb55ce2ee3165c06e94785
Justin Ly <justin.ly016@gmail.com>
2015-07-06 02:13:32 -0700

    Modified database settings

--- tunak_server/settings.py
+++ tunak_server/settings.py
@@ -75,17 +75,13 @@ WSGI_APPLICATION = 'tunak_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-import dj_database_url
 DATABASES = {
-    "default": dj_database_url.config(),
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
