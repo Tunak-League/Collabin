7b0aa91d89ddc6178e1b70609d17aaad0386c2a2
Jun Hyung Park <junhyungandrewpark@gmail.com>
2015-07-07 00:08:03 -0700

    added and updated models

--- server/models.py
+++ server/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 
-# Create your models here.
 class Users(models.Model):
-	last_name = models.CharField(max_length=30)
-	first_name = models.CharField(max_length=30)
-	email = models.CharField(max_length=30)
-	password = models.CharField(max_length=20)
-	user_summary = models.CharField(max_length=500)
-	location = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    user_summary = models.CharField(max_length=500)
+    location = models.CharField(max_length=30)
 
 class Skills(models.Model):
-	skill_name = models.CharField(max_length=30)
-	users = models.ManyToManyField(Users)
+    skill_name = models.CharField(max_length=30)
+    users = models.ManyToManyField(Users)
+    projects = models.ManyToManyField(Projects)
 
 class Types(models.Model):
-	type_name = models.CharField(max_length=30)
-class Users_skills(models.Model):
+    type_name = models.CharField(max_length=30)
     users = models.ManyToManyField(Users)
-    skills = models.ManyToManyField(Skills)
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=30)
+    project_summary = models.CharField(max_length=500)
+    date_created = models.DateField()
+    id_types = models.ManyToManyField(Types)
+    id_owner = models.ManyToManyField(Users)
 
