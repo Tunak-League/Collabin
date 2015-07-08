a6f10a0fec5e695b61e6bce192ed7975b823ccd3
Justin Ly <justin.ly016@gmail.com>
2015-07-07 19:38:02 -0700

    Modified models. Added image_path field to user and projects

--- server/models.py
+++ server/models.py
@@ -7,20 +7,21 @@ class Users(models.Model):
     password = models.CharField(max_length=20)
     user_summary = models.CharField(max_length=500)
     location = models.CharField(max_length=30)
+    image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
 
 class Skills(models.Model):
     skill_name = models.CharField(max_length=30)
-    users = models.ManyToManyField(Users)
-    projects = models.ManyToManyField(Projects)
+    users = models.ManyToManyField('Users')
+    projects = models.ManyToManyField('Projects')
 
 class Types(models.Model):
     type_name = models.CharField(max_length=30)
-    users = models.ManyToManyField(Users)
+    users = models.ManyToManyField('Users')
 
 class Projects(models.Model):
     project_name = models.CharField(max_length=30)
     project_summary = models.CharField(max_length=500)
     date_created = models.DateField()
-    id_types = models.ManyToManyField(Types)
-    id_owner = models.ManyToManyField(Users)
-
+    id_types = models.ForeignKey('Types')
+    id_owner = models.ForeignKey('Users')
+    image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
--- tunak_server/settings.py
+++ tunak_server/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'tunak_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
+import dj_database_url
+DATABASES = {}
+DATABASES['default']  =  dj_database_url.config()
+#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 
 # Internationalization
