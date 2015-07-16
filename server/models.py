86ed181bc429c458b991ab643e9f4df239e42f5b
Justin Ly <justin.ly016@gmail.com>
2015-07-16 01:31:20 -0700

    Started implementing Token Authentication. Users can request a token now

--- server/models.py
+++ server/models.py
@@ -24,3 +24,13 @@ class Projects(models.Model):
     id_types = models.ForeignKey('Types')
     id_owner = models.ForeignKey('UserProfiles')
     image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
+
+from django.conf import settings 
+from django.db.models.signals import post_save 
+from django.dispatch import receiver 
+from rest_framework.authtoken.models import Token 
+ 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL) 
+def create_auth_token(sender, instance=None, created=False, **kwargs): 
+    if created: 
+        Token.objects.create(user=instance) 
