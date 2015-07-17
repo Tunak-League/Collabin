44c2129fb5d2d2f277c75aeb0f99fef119229dc5
Justin Ly <justin.ly016@gmail.com>
2015-07-17 12:09:30 -0700

    Working on the project-list endpoint

---
+++ server/serializers.py
@@ -0,0 +1,35 @@
+from django.forms import widgets
+from rest_framework import serializers
+from server.models import UserProfiles, Skills, Types, Projects
+from django.contrib.auth.models import User
+'''
+class UsersSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'first_name', 'last_name', 'username', 'password', 'user_permissions')
+
+class UserProfilesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserProfiles
+        fields = ('url', 'user_summary', 'location', 'image_path')
+
+class SkillsSerializer(serializers.HyperlinkedModelSerializer):
+    users = serializers.HyperlinkedRelatedField(many = True, view_name = '', read_only=True)
+    projects = serializers.HyperlinkedRelatedField(many = True, view_name = '', read_only=True)
+    class Meta:
+        model = Skills
+        fields = ('url', 'skill_name', 'users', 'projects')
+
+class TypesSerializer(serializers.HyperlinkedModelSerializer):
+    users = serializers.HyperlinkedRelatedField(many = True, view_name = '', read_only=True)
+    class Meta:
+        model = Types
+        fields = ('url', 'type_name', 'users')
+'''
+class ProjectsSerializer(serializers.HyperlinkedModelSerializer):
+  #  owner = serializers.HyperlinkedRelatedField(many = False, view_name = '', read_only=True) 
+    types = serializers.ReadOnlyField(source = 'id_types.type_name')
+    class Meta:
+        model = Projects
+        fields = ('url', 'project_name', 'project_summary', 'date_created', 'image_path','types' ) # 'owner')
+
