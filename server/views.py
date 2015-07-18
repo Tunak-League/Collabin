263efd808a00326ffda2c51b78ac3943a5ffd29d
Justin Ly <justin.ly016@gmail.com>
2015-07-18 00:30:50 -0700

    Removed debugging print statements

--- server/views.py
+++ server/views.py
@@ -18,14 +18,12 @@ class ProjectList(APIView):
         stored in the database
     '''
     def get(self, request, format=None):
-        print "HALP ME:" + request.user.username
         profile = UserProfiles.objects.get(user_id=request.user.id) #obtain UserProfile from the requesting User's id 
         types_profiles = Types.users.through #the types_profiles pivot table
         preferredTypes = types_profiles.objects.filter( userprofiles_id= profile.id ) #obtain all Types preferred by the requesting user
         preferredTypes = [x.types_id for x in preferredTypes] 
-
         projects = Projects.objects.filter( id_types__in=preferredTypes ) #obtain all projects with a type matching an item in preferredTypes
-        print "HEY OVER HERE:" + projects[3].project_name
+
         #Serialize the data and return it
         serializer = ProjectsSerializer(projects, many=True, context={'request': request})
         return Response( serializer.data )
