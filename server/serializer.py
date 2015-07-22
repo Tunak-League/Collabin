a3208ac143b15101e2c184d68ab058a586529107
carol975 <carol975@users.noreply.github.com>
2015-07-22 14:54:45 -0700

    Update serializer.py

--- server/serializer.py
+++ server/serializer.py
@@ -6,30 +6,29 @@ from django.contrib.auth.models import User
 class UsersSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields('url', 'first_name', 'last_name', 'username', 'password', 'user_permissions')
+        fields = ('url', 'first_name', 'last_name', 'username', 'password', 'user_permissions')
 
 class UserProfilesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserProfiles
-        fields('url', 'user_summary', 'location', 'image_path')
+        fields = ('url', 'user_summary', 'location', 'image_path')
 
 class SkillsSerializer(serializers.HyperlinkedModelSerializer):
     users = serializers.HyperlinkedRelatedField(many = True, view_name = '')
     projects = serializers.HyperlinkedRelatedField(many = True, view_name = '')
     class Meta:
         model = Skills
-        fields('url', 'skill_name', 'users', 'projects')
+        fields = ('url', 'skill_name', 'users', 'projects')
 
 class TypesSerializer(serializers.HyperlinkedModelSerializer):
     users = serializers.HyperlinkedRelatedField(many = True, view_name = '')
     class Meta:
         model = Types
-        fields('url', 'type_name', 'users')
+        fields = ('url', 'type_name', 'users')
 
 class ProjectsSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.HyperlinkedRelatedField(many = False, view_name = '') 
     types = serializers.ReadOnlyField(source = 'owner.username')
     class Meta:
         model = Projects
-        fields('url', 'project_name', 'project_summary', 'date_created', 'image_path', 'types', 'owner')
-
+        fields = ('url', 'project_name', 'project_summary', 'date_created', 'image_path', 'types', 'owner')
