68a8125b6ecac864e99eea73fb6e8930d15f93df
ajhpark <junhyungandrewpark@gmail.com>
2015-07-22 16:41:45 -0700

    Update serializers.py
    changed from HyperlinkedModelSerializers to ModelSerializers

--- server/serializers.py
+++ server/serializers.py
@@ -3,34 +3,35 @@ from rest_framework import serializers
 from server.models import UserProfiles, Skills, Types, Projects
 from django.contrib.auth.models import User
 
-class UsersSerializer(serializers.HyperlinkedModelSerializer):
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'first_name', 'last_name', 'username', 'password', 'user_permissions')
+        fields = ('id', 'first_name', 'last_name', 'username', 'password', 'user_permissions')
 
-class UserProfilesSerializer(serializers.HyperlinkedModelSerializer):
+class UserProfilesSerializer(serializers.ModelSerializer):
+    skills = serializers.SlugRelatedField (
+        many = True,
+        read_only = True,
+        slug_field = 'skill_name',
+    )
     class Meta:
         model = UserProfiles
-        fields = ('url', 'user_summary', 'location', 'image_path')
+        fields = ('id', 'user_summary', 'location', 'image_path', 'skills')
 
-class SkillsSerializer(serializers.HyperlinkedModelSerializer):
+class SkillsSerializer(serializers.ModelSerializer):
     users = serializers.HyperlinkedRelatedField(many = True, view_name = '', read_only=True)
     projects = serializers.HyperlinkedRelatedField(many = True, view_name = '', read_only=True)
     class Meta:
         model = Skills
-        fields = ('url', 'skill_name', 'users', 'projects')
+        fields = ('id', 'skill_name', 'users', 'projects')
 
-class TypesSerializer(serializers.HyperlinkedModelSerializer):
+class TypesSerializer(serializers.ModelSerializer):
     users = serializers.HyperlinkedRelatedField(many = True, view_name = '', read_only=True)
     class Meta:
         model = Types
-        fields = ('url', 'type_name', 'users')
+        fields = ('id', 'type_name', 'users')
 
 class ProjectsSerializer(serializers.ModelSerializer):
-  #  owner = serializers.HyperlinkedRelatedField(many = False, view_name = '', read_only=True) #NO VIEW RIGHT NOW, COMMENT OUT UNTIL THEN
-   # url = serializers.HyperlinkedIdentityField(view_name="project-list"  )
-    #typeList = [x.type_name for x in list(self.instance.types) ]
-    #types = serializers.ReadOnlyField(source = 'types', many=True)
     types = serializers.SlugRelatedField(
         many=True,
         queryset=Projects.types,
@@ -38,5 +39,5 @@ class ProjectsSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = Projects
-        fields = (   'id','project_name', 'project_summary', 'id_owner', 'date_created', 'image_path','types' ) # 'owner')
+        fields = ('id', 'project_name', 'project_summary', 'owner', 'date_created', 'image_path','types')
 
