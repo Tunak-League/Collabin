0bb9bc088dfed87efb4cae1e2d061a2e977b4a6c
carol975 <carol975@users.noreply.github.com>
2015-07-22 13:56:31 -0700

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import url
-from . import views
+from server import views
+from rest_frameswork.urlpatterns import format_suffix_patterns
 
+
+# API endpoints
 urlpatterns = [
-    url(r'^$', views.index, name = 'index')
+    url(r'^$', views.api_root),
+	url(r'^server/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^server/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='User-detail'),
 ]
 
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
