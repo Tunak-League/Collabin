b0220adf6098135b462ad02314c30982db4e33ec
ajhpark <junhyungandrewpark@gmail.com>
2015-07-22 16:50:18 -0700

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from server import views
 
-urlpatterns = [
-    #url(r'^$', views.index, name = 'index')
+urlpatterns = format_suffix_patterns([
     url(r'^project-search/', views.ProjectSearch.as_view(), name='project-search' ),
-    url(r'^project/', views.Project.as_view(), name='project' ),
-]
+    url(r'^project/', views.Project.as_view(), name='project'),
+    url(r'^user-search/(?P<pk>[0-9]+)/$', views.UserSearch.as_view()),
+])
 
