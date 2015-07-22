7dd87fcaa30f23edfa4dd4598997f8300df473c8
carol975 <carol975@users.noreply.github.com>
2015-07-22 13:55:56 -0700

    parts that is working, will upload the rest once everything is fixed

--- server/views.py
+++ server/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render
-from server.models import Projects, UserProfiles
-from server.serializers import ProjectsSerializer
-from rest_framework import generics
 
-class UserList(generics.ListAPIView):
+from server.models import Projects, UserProfiles
+from server.serializers import ProjectsSerializer, UsersSerializer, UserProfilesSerializer
+
+from rest_framework import generics
+from rest_framework import permissions, authentication
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class UserList(generics.ListCreateAPIView)
+	authentication_classes = (TokenAuthentication)
+	permission_classes = (IsAuthenticated,)
+	queryset = User.objects.all()
+	serializer_class = UsersSerializer
+	
+	def post(self, request, format=None):
+		content = {
+			 'user': unicode(request.user),  # `django.contrib.auth.User` instance.
+            		'auth': unicode(request.auth),  # None
+      		  }
+        	return Response(content)
+	
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+	authentication_classes = (TokenAuthentication)
+	permission_classes = (IsAuthenticated,)
+	queryset = UserProfiles.objects.all()
+	serializer_class = UsersSerializer
+
+	def get(self, request, format=None):
+		content = {
+			 'user': unicode(request.user),  # `django.contrib.auth.User` instance.
+            		'auth': unicode(request.auth),  # None
+      		  }
+        	return Response(content)
+	
+
+@api_view(('GET',))
+def api_root(request, format=None):
+	return Response({'users' : reverse('user-list', request=request, format=format),
+			'project': reverse('project-list', request=request, format=format)})
+
+
 
