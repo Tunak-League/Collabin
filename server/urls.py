9f823f1fb9a91cfb3a292a16cc474a7217dd891f
carol975 <carol975@users.noreply.github.com>
2015-07-23 18:43:49 -0700

    Update urls.py
    no token auth

--- server/urls.py
+++ server/urls.py
@@ -4,15 +4,22 @@ from rest_frameswork.urlpatterns import format_suffix_patterns
 
 
 # API endpoints
-urlpatterns = [
+urlpatterns = ([
     url(r'^$', views.api_root),
-	url(r'^server/$',
-        views.UserList.as_view(),
-        name='user-list'),
-    url(r'^server/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='User-detail'),
-]
+    url(r'^server/$', views.UserList.as_view(), name='user-list'),
+    url(r'^server/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+
+    url(r'^server/$', views.SkillsList.as_view(), name='skills-list'),
+    url(r'^server/(?P<pk>[0-9]+)/$', views.SkillsDetail.as_view(), name='skills-detail'),
+
+    url(r'^server/$', views.TypesList.as_view(), name='types-list'),
+    url(r'^server/(?P<pk>[0-9]+)/$', views.TypesDetail.as_view(), name='types-detail'),
+
+    url(r'^server/$', views.ProjectsList.as_view(), name='projects-list'),
+    url(r'^server/(?P<pk>[0-9]+)/$', views.ProjectsDetail.as_view(), name='projects-detail'),
+    
+
+])
 
 # Login and logout views for the browsable API
 urlpatterns += [
