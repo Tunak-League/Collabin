d4948bb3181c56b0ef75669c9998953d4474288c
carol975 <carol975@users.noreply.github.com>
2015-07-23 18:42:37 -0700

    Update models.py
    no token auth

--- server/models.py
+++ server/models.py
@@ -25,6 +25,7 @@ class Projects(models.Model):
     id_owner = models.ForeignKey('UserProfiles')
     image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
 
+"""
 from django.conf import settings 
 from django.db.models.signals import post_save 
 from django.dispatch import receiver 
@@ -34,3 +35,4 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs): 
     if created: 
         Token.objects.create(user=instance) 
+"""
