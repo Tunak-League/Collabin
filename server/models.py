5ae957306e77e051e3a4531e5651062703190198
Jun Hyung Park <junhyungandrewpark@gmail.com>
2015-07-25 15:29:54 -0700

    added swipes view and serializer

--- server/models.py
+++ server/models.py
@@ -6,29 +6,35 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token 
 
 class UserProfiles(models.Model):
-    user_summary = models.CharField(max_length=500)
-    location = models.CharField(max_length=30)
+    user_summary = models.CharField(max_length = 500)
+    location = models.CharField(max_length = 30)
     image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)	
 
 class Skills(models.Model):
-    skill_name = models.CharField(max_length=30, unique = True)
-    user_profiles = models.ManyToManyField('UserProfiles', related_name="skills")
-    projects = models.ManyToManyField('Projects', related_name="skills")
+    skill_name = models.CharField(max_length = 30, unique = True)
+    user_profiles = models.ManyToManyField('UserProfiles', related_name = "skills")
+    projects = models.ManyToManyField('Projects', related_name = "skills")
 
 class Types(models.Model):
-    type_name = models.CharField(max_length=30, unique = True)
-    user_profiles = models.ManyToManyField('UserProfiles', related_name="types")
-    projects = models.ManyToManyField('Projects', related_name="types")
+    type_name = models.CharField(max_length = 30, unique = True)
+    user_profiles = models.ManyToManyField('UserProfiles', related_name = "types")
+    projects = models.ManyToManyField('Projects', related_name = "types")
 
 class Projects(models.Model):
-    project_name = models.CharField(max_length=30, unique = True)
-    project_summary = models.CharField(max_length=500)
+    project_name = models.CharField(max_length = 30, unique = True)
+    project_summary = models.CharField(max_length = 500)
     date_created = models.DateField(null = True, blank = True, default = None)
     owner = models.ForeignKey('UserProfiles')
-    image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
- 
+    image_path = models.CharField(max_length = 50, null = True, blank = True, default = None)
+
+class Swipes(models.Model):
+    user_profile = models.ForeignKey('UserProfiles')
+    project = models.ForeignKey('Projects')
+    user_likes = models.CharField(max_length = 3, null = True, blank = True, default = None)
+    project_likes = models.CharField(max_length = 3, null = True, blank = True, default = None)
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL) 
-def create_auth_token(sender, instance=None, created=False, **kwargs): 
+def create_auth_token(sender, instance = None, created = False, **kwargs): 
     if created: 
-        Token.objects.create(user=instance) 
+        Token.objects.create(user = instance) 
