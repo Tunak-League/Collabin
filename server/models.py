a77eb09922c997a9f1114a6d2b75ae75a0bb14df
carol975 <carol975@users.noreply.github.com>
2015-07-26 00:34:54 -0700

    Update models.py

--- server/models.py
+++ server/models.py
@@ -9,23 +9,24 @@ class UserProfiles(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)	
 
 class Skills(models.Model):
-    skill_name = models.CharField(max_length=30)
-    users = models.ManyToManyField('UserProfiles')
-    projects = models.ManyToManyField('Projects')
+    skill_name = models.CharField(max_length=30, unique = True)
+    users_profiles = models.ManyToManyField('UserProfiles', related_name = "skills")
+    projects = models.ManyToManyField('Projects', related_name = "skills")
 
 class Types(models.Model):
     type_name = models.CharField(max_length=30)
-    users = models.ManyToManyField('UserProfiles')
+    user_profiles = models.ManyToManyField('UserProfiles', related_name = "types")
+    projects = models.ManyToManyField('Projects', related_name = "types")
 
 class Projects(models.Model):
     project_name = models.CharField(max_length=30)
     project_summary = models.CharField(max_length=500)
-    date_created = models.DateField()
-    id_types = models.ForeignKey('Types')
-    id_owner = models.ForeignKey('UserProfiles')
+    date_created = models.DateField(null = True, blank = True, default = None)
+    owner = models.ForeignKey('UserProfiles')
     image_path = models.CharField(max_length=50, null = True, blank = True, default = None)
 
-"""
+
+
 from django.conf import settings 
 from django.db.models.signals import post_save 
 from django.dispatch import receiver 
@@ -35,4 +36,3 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs): 
     if created: 
         Token.objects.create(user=instance) 
-"""
