81aa372e943889344bbf3f545bb13fcf5627f985
Justin Ly <justin.ly016@gmail.com>
2015-07-26 22:45:13 -0700

    Implemented project_matches endpoint to return all matched projects for a user

--- server/urls.py
+++ server/urls.py
@@ -7,5 +7,6 @@ urlpatterns = format_suffix_patterns([
     url(r'^project-list/', views.ProjectList.as_view(), name='project-list'),
     url(r'^project-detail/(?P<pk>[0-9]+)/$', views.ProjectDetail.as_view(), name='project-detail'),
     url(r'^user-search/(?P<pk>[0-9]+)/$', views.UserSearch.as_view()),
+    url(r'^project-matches/', views.project_matches, name='project-matches'),
 ])
 
