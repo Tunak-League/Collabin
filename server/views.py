bfba23f012631653e49ab6dae4838491ed363079
carol975 <carol975@users.noreply.github.com>
2015-07-26 19:49:17 -0700

    Update views.py

--- server/views.py
+++ server/views.py
@@ -1,48 +1,52 @@
-
-from django.shortcuts import render
-from django.http import Http404
-from django.db.models import Count
-
 from django.contrib.auth.models import User
 from server.models import UserProfiles, Skills, Types, Projects
 from server.serializer import UsersSerializer, SkillsSerializer, UserProfilesSerializer, TypesSerializer, ProjectsSerializer
 
-from rest_framework import generics
+from django.http import Http404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framwork import status
+from rest_framework import status
 
-
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-import django_filters
-
-#returns the specified user
-class UserSearch(django_filters.FilterSet):
-
-#only users can search for other users
-	authentication_classes = (TokenAuthentication,)
-	permission_classes = (IsAuthenticated,)
-
-#reference: http://www.django-rest-framework.org/api-guide/filtering/#searchfilter
-	queryset = User.objects.all()
-	serializer = UserSerializer
-	filter_bakends = (filters.SearchFilter,)
-	search_fields = ('username','first_name', 'last_name')
-	
-
-		
-from rest_framework import mixins
-from rest_framwork import generics
-
-class Users(generics.GenericAPIVIew):
-	queryset = User.objects.all()
-	serializer_class = UsersProfilesSerializer
+class Users(APIView):
+	"""
+	list the user or create a new user
+	"""
+	def get(self, request, format=None):
+		user = User.objects.all()
+		serializer = UsersSerializer(user)
+		return Response(serializer.data)
 	
 	def post(self, request, format=None):
-		serializer= UsersProfilesSerializer(data=request.data)
+		serializer = UsersSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+	"""
+	Retrieve, update or delete a user
+	"""
+	def get_object(self, pk):
+		try:
+			return User.objects.get(pk=pk)
+		except User.DoesNotExist:
+			raise Http404
+	def get(self, reqeust, pk, format=None):
+		user = self.get_object(pk)
+		serializer = UserSerializer(user)
+		return Response(serializer.data)
+	def put(self, request, pk , format=NOne):
+		user = self.get_object(pk)
+		serializer = UserSerializer(user.data=request.data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		return Response(serialzier.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def delete(self, request, pk, format=None):
+		user = self.get_object(pk)
+		user.delete()
+		return Response(status=status.HTTP_204_N0_CONTENT)
