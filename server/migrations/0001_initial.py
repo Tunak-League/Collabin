b39e57c2077d87a181e2af65116241cf11cba679
Jun Hyung Park <junhyungandrewpark@gmail.com>
2015-07-27 16:51:04 -0700

    user PUT working

--- server/migrations/0001_initial.py
+++ server/migrations/0001_initial.py
@@ -16,9 +16,9 @@ class Migration(migrations.Migration):
             name='Projects',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('project_name', models.CharField(max_length=30)),
+                ('project_name', models.CharField(unique=True, max_length=30)),
                 ('project_summary', models.CharField(max_length=500)),
-                ('date_created', models.DateField()),
+                ('date_created', models.DateField(default=None, null=True, blank=True)),
                 ('image_path', models.CharField(default=None, max_length=50, null=True, blank=True)),
             ],
         ),
@@ -26,15 +26,24 @@ class Migration(migrations.Migration):
             name='Skills',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('skill_name', models.CharField(max_length=30)),
+                ('skill_name', models.CharField(unique=True, max_length=30)),
                 ('projects', models.ManyToManyField(related_name='skills', to='server.Projects')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Swipes',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user_likes', models.CharField(default=None, max_length=3, null=True, blank=True, choices=[(b'YES', b'YES'), (b'NO', b'NO'), (b'?', b'UNKNOWN')])),
+                ('project_likes', models.CharField(default=None, max_length=3, null=True, blank=True, choices=[(b'YES', b'YES'), (b'NO', b'NO'), (b'?', b'UNKNOWN')])),
+                ('project', models.ForeignKey(to='server.Projects')),
             ],
         ),
         migrations.CreateModel(
             name='Types',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('type_name', models.CharField(max_length=30)),
+                ('type_name', models.CharField(unique=True, max_length=30)),
                 ('projects', models.ManyToManyField(related_name='types', to='server.Projects')),
             ],
         ),
@@ -52,6 +61,11 @@ class Migration(migrations.Migration):
             model_name='types',
             name='user_profiles',
             field=models.ManyToManyField(related_name='types', to='server.UserProfiles'),
+        ),
+        migrations.AddField(
+            model_name='swipes',
+            name='user_profile',
+            field=models.ForeignKey(to='server.UserProfiles'),
         ),
         migrations.AddField(
             model_name='skills',
