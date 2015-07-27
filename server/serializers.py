4ef54dcb652289f764265546b2641e985b27e681
Justin Ly <justin.ly016@gmail.com>
2015-07-26 22:44:36 -0700

    Added ProjectMatchSerializer and UserMatchSerializer

--- server/serializers.py
+++ server/serializers.py
@@ -49,12 +49,27 @@ class ProjectsSerializer(serializers.ModelSerializer):
         queryset = Skills.objects.all(),
         slug_field = 'skill_name',
     )
+    owner_name = serializers.ReadOnlyField( source='owner.user.username' )
     class Meta:
         model = Projects
-        fields = ('id', 'project_name', 'project_summary', 'owner', 'date_created', 'image_path','types', 'skills')
+        fields = ('id', 'project_name', 'project_summary', 'owner', 'date_created', 'image_path','types', 'skills', 'owner_name')
         read_only_fields = ('owner',)
 
 class SwipesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Swipes
         fields = ('id', 'user_profile', 'project', 'user_likes', 'project_likes')
+
+class ProjectMatchSerializer( serializers.ModelSerializer ):
+    project_name = serializers.ReadOnlyField( source='project.project_name' )
+    owner = serializers.ReadOnlyField( source='project.owner.user.username')
+    class Meta:
+        model = Swipes
+        fields = ('project', 'project_name', 'owner' ) 
+
+class UserMatchSerializer( serializers.ModelSerializer ):
+    username = serializers.ReadOnlyField( source='user_profile.user.username' )
+    project_name = serializers.ReadOnlyField(source='project.project_name')
+    class Meta:
+        model = Swipes
+        fields = ('user_profile', 'username', 'project_name' )
