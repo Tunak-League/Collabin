584adea31bdc458f0abb0a96161702e546f0aef0
Justin Ly <justin.ly016@gmail.com>
2015-08-07 00:49:11 -0700

    Implemented navigation drawer with a Logout button

--- Redemption/app/src/main/java/tunakleague/com/redemption/HomeActivity.java
+++ Redemption/app/src/main/java/tunakleague/com/redemption/HomeActivity.java
@@ -1,17 +1,95 @@
 package tunakleague.com.redemption;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import tunakleague.com.redemption.authentication.WelcomeActivity;
+
 
 public class HomeActivity extends AppCompatActivity {
+    public static final String TAG = "HomeActivity";
+
+    private String[] navigation_options;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+
+
+    /*Constants for items in the navigation drawer*/
+    public static final int LOGOUT = 0;
+    public static final int PROFILE = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navigation_options = getResources().getStringArray(R.array.navigation_options);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, navigation_options));
+
+        /*Specify which actions to take when an item is selected from the navigation drawer*/
+        mDrawerList.setOnItemClickListener( new ListView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position) {
+                    case LOGOUT:
+                        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(HomeActivity.this);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.remove(PreferencesKeys.AUTH_TOKEN); //Clear the token
+                        editor.commit();
+
+//                       /*Return to the welcome page*/
+                        Intent welcomeIntent = new Intent( HomeActivity.this, WelcomeActivity.class );
+                        startActivity(welcomeIntent);
+                        break;
+                    case PROFILE:
+                        Log.d(TAG, "GOING TO THE PROFILE NOW");
+                        break;
+                    default:
+                        Log.d(TAG, "WHAT DID YOU CLICK?????");
+                }
+            }
+        });
+
+
+
+         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                 R.string.drawer_open, R.string.drawer_close) {
+
+
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,18 +98,32 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
+        // Handle your other action bar items...
 
         return super.onOptionsItemSelected(item);
     }
+
 }
