f02ecf0fb7f3bfd9685b263e921459d69b3789e3
Justin Ly <justin.ly016@gmail.com>
2015-08-15 00:41:56 -0700

    Added reusable function to return token header for authentication

--- Redemption/app/src/main/java/tunakleague/com/redemption/MyApplication.java
+++ Redemption/app/src/main/java/tunakleague/com/redemption/MyApplication.java
@@ -2,9 +2,15 @@ package tunakleague.com.redemption;
 
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyApplication extends Application {
     public static RequestQueue requestQueue; //global queue for making http requests
@@ -13,6 +19,19 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         requestQueue = Volley.newRequestQueue(getApplicationContext());
+    }
+
+    /*
+    Returns a map containing the Authentication header with the token for the user.
+    @requires: SharedPreferences must contain a valid K-V pair
+     */
+    public static Map<String,String> getAuthenticationHeader( Context context ) {
+        Map<String, String>  params = new HashMap<>();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+
+        params.put("Authorization", "Token " + settings.getString(PreferencesKeys.AUTH_TOKEN, "noToken") );
+        //TODO: Put in the token header
+        return params;
     }
 
 
