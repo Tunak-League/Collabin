1eee9b600cc18bbd8b1bb956e6b6c8750e0ca62e
Justin Ly <justin.ly016@gmail.com>
2015-08-22 02:43:06 -0700

    Refactored activities in authentication to extend and reuse AuthenticationActivity and DeviceIDReceiver

---
+++ Redemption/app/src/main/java/tunakleague/com/redemption/authentication/DeviceIDReceiver.java
@@ -0,0 +1,34 @@
+package tunakleague.com.redemption.authentication;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import tunakleague.com.redemption.Constants;
+import tunakleague.com.redemption.notifications.NotificationsPreferences;
+
+
+public class DeviceIDReceiver extends BroadcastReceiver {
+    private String deviceID;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /*Abort and do nothing if the intent's action is not REGISTRATION_COMPLETE */
+        if(!intent.getAction().equals(NotificationsPreferences.REGISTRATION_COMPLETE)) {
+            return;
+
+        }
+        LocalBroadcastManager.getInstance(context).unregisterReceiver(this); //unregister this receiver once the deviceID has been obtained
+
+        deviceID = intent.getExtras().getString(Constants.DEVICE_ID); //Device id obtained from GCM
+    }
+
+    /*Return the obtained device ID. Will be null if called before onReceive() has finished executing*/
+    public String getDeviceID() {
+        return deviceID;
+    }
+}
