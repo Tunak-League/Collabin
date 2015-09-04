ad79d2f922f0437c5fa8319ec7c55d21de35cdd1
Jun Hyung Park <junhyungandrewpark@gmail.com>
2015-09-03 18:08:23 -0700

    fix password confirm

--- Redemption/app/src/main/java/tunakleague/com/redemption/messaging/ChatProfileFragment.java
+++ Redemption/app/src/main/java/tunakleague/com/redemption/messaging/ChatProfileFragment.java
@@ -2,19 +2,16 @@ package tunakleague.com.redemption.messaging;
 
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,8 +19,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import tunakleague.com.redemption.MyApplication;
-import tunakleague.com.redemption.app_constants.PreferencesKeys;
 import tunakleague.com.redemption.R;
+import tunakleague.com.redemption.app_constants.PreferencesKeys;
 import tunakleague.com.redemption.app_constants.ServerConstants;
 import tunakleague.com.redemption.experimental.ExpandableHeightGridView;
 import tunakleague.com.redemption.profiles.BaseProfileFragment;
@@ -31,8 +28,6 @@ import tunakleague.com.redemption.profiles.BaseProfileFragment;
 public class ChatProfileFragment extends BaseProfileFragment {
     private View view;
     private int profileId;
-    private TextView userName, email, location, userSummary;
-    private TextView preferredTypes, skills;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,9 +40,9 @@ public class ChatProfileFragment extends BaseProfileFragment {
         initializeImageData((ImageView) view.findViewById(R.id.user_image), ServerConstants.USERS.USER_IMAGE.string);
 
         fieldsToPopulate.put(view.findViewById(R.id.username), ServerConstants.USERS.USERNAME.string);
-        fieldsToPopulate.put( view.findViewById(R.id.email), ServerConstants.USERS.EMAIL.string );
-        fieldsToPopulate.put( view.findViewById(R.id.user_summary), ServerConstants.USERS.USER_SUMMARY.string );
-        fieldsToPopulate.put( view.findViewById(R.id.location), ServerConstants.USERS.LOCATION.string );
+        fieldsToPopulate.put(view.findViewById(R.id.email), ServerConstants.USERS.EMAIL.string);
+        fieldsToPopulate.put(view.findViewById(R.id.user_summary), ServerConstants.USERS.USER_SUMMARY.string );
+        fieldsToPopulate.put(view.findViewById(R.id.location), ServerConstants.USERS.LOCATION.string );
 
         profileId = getActivity().getIntent().getIntExtra("recipient", -1);
         String url = ServerConstants.URLS.USER_GET.string + Integer.toString(profileId) + "/";
