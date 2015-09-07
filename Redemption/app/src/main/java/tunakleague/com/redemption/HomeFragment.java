039c8607ab0f77c21fc7941d2a2fa5b14cce9f35
Jun Hyung Park <junhyungandrewpark@gmail.com>
2015-09-07 15:58:45 -0700

    fix user detail view

--- Redemption/app/src/main/java/tunakleague/com/redemption/HomeFragment.java
+++ Redemption/app/src/main/java/tunakleague/com/redemption/HomeFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment  {
-    private View chatButton;
     private View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
